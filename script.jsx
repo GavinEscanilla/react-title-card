a7d10a34650ed0e3711cdf8ff8514e2175b81cb2
GavinEscanilla <gavinaltonescanilla@gmail.com>
2025-03-21 15:41:35 -0400

    test

--- script.jsx
+++ script.jsx
@@ -6,6 +6,15 @@ const  App = () => {
         </div>
     )
 }
+const TitleCard = () => {
+    const [name,setname] = useState('')
+    const[color,setColor] = useState('')
+    return(  
+        <div id="}
+            <h1>React title card</h1>
+        </div>
+    )
+}
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
